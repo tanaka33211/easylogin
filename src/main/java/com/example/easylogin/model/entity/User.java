34bfc4ed741a90dcfc6371e6971f7ce503197da0
tanaka33211 <yude.tamago5535@gmail.com>
2023-05-08 19:00:43 +0900

    ログインフォーム

---
+++ src/main/java/com/example/easylogin/model/entity/User.java
@@ -0,0 +1,53 @@
+package com.example.easylogin.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="full_name")
+	private String fullName;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+}
